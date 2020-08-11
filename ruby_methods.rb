94e1478a7d6c2b13ae07798d0d22c699d0fbfa06
Muhammad Awais <awaisshaikh94@gmail.com>
2020-08-10 23:52:42 -0700

    array methods

---
+++ ruby_methods.rb
@@ -0,0 +1,13 @@
+def add_nums(num1, num2)
+    return num1+num2;
+end
+
+@sum = add_nums(3, 4)
+puts @sum;
+
+def sorted_reverse(array)
+    return array.reverse();
+end
+
+@arr = sorted_reverse([2, 3, 6, 8])
+puts @arr;

24e9d381ecb66171c169d3834a8e2e866df25c3b
Muhammad Awais <awaisshaikh94@gmail.com>
2020-08-10 23:29:18 -0700

    strings in ruby

---
+++ ruby_strings.rb
@@ -0,0 +1,12 @@
+@name = "Muhammad Awais";
+
+# getting length from string
+puts @name.length;
+
+puts @name[0];
+
+# check if name includes some partial string 
+puts @name.include? "mmad"
+
+# print name with respected indexes
+puts @name[1,3];

86e0108a41551413603fba1476f48586d33b2120
Muhammad Awais <awaisshaikh94@gmail.com>
2020-08-10 23:44:21 -0700

    more in ruby arrays

--- ruby_arrays.rb
+++ ruby_arrays.rb
@@ -4,12 +4,24 @@ puts @lucky_numbers;
 
 puts @lucky_numbers[2];
 
-# including last one
+# # including last one
 puts @lucky_numbers[2,4];
 
-# exclusing last one
+# # exclusing last one
 puts @lucky_numbers[2..4];
 
 puts @lucky_numbers.length;
 
+@friends = [];
 
+@friends.push("Mehroz");
+@friends.push("Farhan");
+@friends.push("Arsalan");
+
+puts @friends;
+
+puts @friends.reverse();
+
+puts @friends.sort();
+
+puts @friends.include? "Arsalan";

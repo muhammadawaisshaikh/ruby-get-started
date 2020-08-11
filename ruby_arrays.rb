ea66852c614dec7fba6bb56fcac69db6f1ee2a69
Muhammad Awais <awaisshaikh94@gmail.com>
2020-08-10 23:39:44 -0700

    ruby arrays

---
+++ ruby_arrays.rb
@@ -0,0 +1,15 @@
+@lucky_numbers = [2, 4, 6, 8, 1, 10];
+
+puts @lucky_numbers;
+
+puts @lucky_numbers[2];
+
+# including last one
+puts @lucky_numbers[2,4];
+
+# exclusing last one
+puts @lucky_numbers[2..4];
+
+puts @lucky_numbers.length;
+
+
